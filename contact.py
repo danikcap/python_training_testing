773fe21d12c3dcbcda63c424c182515137fdeb13
DAN <danikcap@mail.ru>
2024-07-02 19:30:41 +0500

    Задание 3: Задание выполнено

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile,
+                 work, fax, email, email2, homepage, email3, bday, bmonth, byear, aday, amonth, ayear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.homepage = homepage
+        self.email3 = email3
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
