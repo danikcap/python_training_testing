b95f7a42490525948bcc2965c9cc6a498d1e609a
DAN <danikcap@mail.ru>
2024-07-09 19:45:50 +0500

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact("asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd",
                                    "asd", "asd", "asd", "asd", "27", "September", "1900", "26", "November", "1900"))
-    app.logout()
+    app.session.logout()
