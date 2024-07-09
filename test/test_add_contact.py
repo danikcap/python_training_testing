4c21249560f6bba292cd27820bca91b28c078cfa
DAN <danikcap@mail.ru>
2024-07-09 20:11:51 +0500

    Задание 5: Задание выполнено

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,6 +14,6 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact("asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd",
-                                   "asd", "asd", "asd", "asd", "27", "September", "1900", "26", "November", "1900"))
+    app.contact.create(Contact("asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd",
+                               "asd", "asd", "asd", "asd", "27", "September", "1900", "26", "November", "1900"))
     app.session.logout()
