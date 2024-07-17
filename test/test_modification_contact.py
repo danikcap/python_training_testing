ee70e8d4801bd7c66c0d2532037755e6b9666a79
DAN <danikcap@mail.ru>
2024-07-17 17:57:47 +0500

    Задание 9: Задание выполнено

--- test/test_modification_contact.py
+++ test/test_modification_contact.py
@@ -3,6 +3,8 @@ from model.contact import Contact
 
 
 def test_modification_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="3"))
     app.contact.modification_first_contact(Contact("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
                                                    "12", "13", "14", "15", "25", "September", "1800", "20",
                                                    "November", "1700"))
--- test/test_modification_group.py
+++ test/test_modification_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 
 def test_modification_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modification_first_group(Group(name="New group"))
 
 
 def test_modification_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modification_first_group(Group(header="New header"))
