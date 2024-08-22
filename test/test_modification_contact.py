3c179b2cc10f6f69c273ee951f6388df9bdcee8f
DAN <danikcap@mail.ru>
2024-08-22 19:55:50 +0500

    Задание 20: Задание выполнено

--- test/test_modification_contact.py
+++ test/test_modification_contact.py
@@ -4,7 +4,7 @@ import random
 from model.contact import Contact
 
 
-def test_modification_contact(app, db, json_contacts):
+def test_modification_contact(app, db, json_contacts, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="3"))
     old_contacts = db.get_contact_list()
@@ -18,3 +18,6 @@ def test_modification_contact(app, db, json_contacts):
             old_contacts[k] = contact
             break
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)
--- test/test_modification_group.py
+++ test/test_modification_group.py
@@ -4,7 +4,7 @@ import random
 from model.group import Group
 
 
-def test_modification_group_name(app, db, json_groups):
+def test_modification_group_name(app, db, json_groups, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -18,7 +18,8 @@ def test_modification_group_name(app, db, json_groups):
             old_groups[k] = group
             break
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 # def test_modification_group_header(app):
 #     if app.group.count() == 0:
