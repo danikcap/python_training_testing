0eed2806821dcb1704ca0afa44d73b9b2943eaea
DAN <danikcap@mail.ru>
2024-08-26 20:01:39 +0500

    Задание 22: В процессе

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -7,7 +7,7 @@ from model.group import Group
 DB = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
-def test_add_contact(app):
+def test_add_contact_to_group(app):
     if len(DB.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     if len(DB.get_contact_list()) == 0:
