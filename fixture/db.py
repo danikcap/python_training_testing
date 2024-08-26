d2f8d5ea9282f1acbc6844987cc44e38e0ccf167
DAN <danikcap@mail.ru>
2024-08-26 18:57:06 +0500

    Задание 21: Задание выполнено

--- fixture/db.py
+++ fixture/db.py
@@ -27,11 +27,13 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated is NULL or "
-                           "deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3 "
+                           "from addressbook where deprecated is NULL or deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,
+                                    home_phone=home, mobile_phone=mobile, work_phone=work, email=email, email2=email2,
+                                    email3=email3))
         finally:
             cursor.close()
         return list
--- test/test_all_contact_fields.py
+++ test/test_all_contact_fields.py
@@ -4,6 +4,21 @@ from random import randrange
 from model.contact import Contact
 
 
+def test_all_contact_fields(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+                                   "25", "September", "1800", "20", "November", "1700"))
+    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(db.get_contact_list())):
+        assert contact_from_home_page[i].lastname == contact_from_db[i].lastname
+        assert contact_from_home_page[i].firstname == contact_from_db[i].firstname
+        assert contact_from_home_page[i].address == contact_from_db[i].address
+        assert contact_from_home_page[i].all_emails == merge_emails_like_on_home_page(contact_from_db[i])
+        assert contact_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(
+            contact_from_db[i])
+
+
 def test_all_random_contact_fields(app):
     contact_count = app.contact.count()
     if contact_count == 0:
